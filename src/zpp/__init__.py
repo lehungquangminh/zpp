4803a16471cf2418a6c18af43a1d7f8c4e39211e
Le Hung Quang Minh <lhquangmink@gmail.com>
2025-09-14 23:23:49 +0700

    chore: set self-update to zynpp; minor stability tweaks; bump 2.0.2

--- src/zpp/__init__.py
+++ src/zpp/__init__.py
@@ -5,8 +5,9 @@ Exposes the package version and common paths.
 
 from __future__ import annotations
 
-__all__ = ["__version__"]
+__all__ = ["__version__", "DIST_NAME"]
 
 __version__ = "0.1.0"
+DIST_NAME = "zynpp"
 
 
